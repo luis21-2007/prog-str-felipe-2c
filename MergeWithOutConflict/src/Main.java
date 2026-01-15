f03cc6da7c93dc32d4bdba8cea7f9fee7eb7d890
luis21-2007 <20253ds069@utez.edu.mx>
2026-01-14 22:16:15 -0600

    feat: agregar operaciones

--- MergeWithOutConflict/src/Main.java
+++ MergeWithOutConflict/src/Main.java
@@ -3,11 +3,8 @@
 void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Semana 2"));
+    IO.println(String.format("2 + 2 = 4"));
 
-
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("Version base");
+    IO.println("5 * 3 = 15");
 
 }
